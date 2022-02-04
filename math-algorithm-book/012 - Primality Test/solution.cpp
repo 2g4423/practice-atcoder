413a0ee2ce372b343e170a32f2a355ae97d435c4
44232g <yoshifumi.tsuji.biz@gmail.com>
2022-02-04 10:04:20 +0900

    Done 012

---
+++ math-algorithm-book/012 - Primality Test/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isprime(long long n) {
+    for(long long i = 2; i * i <= n; i++) {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n; 
+    cin >> n;
+
+    if(isprime(n) == true) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
