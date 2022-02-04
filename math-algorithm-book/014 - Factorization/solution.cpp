c2f6f78de9fee35a6f8277916cf48c5197ae71e1
44232g <yoshifumi.tsuji.biz@gmail.com>
2022-02-04 12:20:35 +0900

    Done 014

---
+++ math-algorithm-book/014 - Factorization/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n; 
+    cin >> n;
+
+    long long proc = n;
+    for(long long i = 2; i * i <= n; i++) {
+        while(n % i == 0) {
+            n /= i;
+            cout << i << endl;
+        }
+    }
+    if(n >= 2) cout << n << endl;
+
+    return 0;
+}
